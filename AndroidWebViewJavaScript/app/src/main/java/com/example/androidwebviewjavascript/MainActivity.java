14e18bf832495b91aae9fc905c219956c6c01a8a
HSUN <hsg210@gmail.com>
2023-11-27 11:06:40 +0800

    Add files via upload
    first commit

---
+++ AndroidWebViewJavaScript/app/src/main/java/com/example/androidwebviewjavascript/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.androidwebviewjavascript;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.LifecycleRegistry;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String JS_NAME = "JsTest";
+    private Toolbar mToolbar;
+    private Button bCallJs;
+    private WebView webView;
+    private boolean lockBackKey;
+    private LifecycleRegistry mLifecycleRegistry;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mToolbar);
+
+        bCallJs = findViewById(R.id.callJs);
+        bCallJs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+                builder.setMessage("This is a message.");
+                builder.setTitle("This is a title.");
+                AlertDialog dialog = builder.create();
+                dialog.show();*/
+
+                webView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        webView.loadUrl("javascript:calledFromAndroid('msg from Android Java')");
+                    }
+                });
+            }
+        });
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        // 設定與Js互動的權限
+        webSettings.setJavaScriptEnabled(true);
+        // 設定允許JS彈窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.addJavascriptInterface(new JsInterface(this), JS_NAME);
+        // 步驟1：載入JS程式碼
+        // 格式規定為:file:///android_asset/檔案名稱.html
+        webView.loadUrl("file:///android_asset/jsinteract.html");
+        mLifecycleRegistry = new LifecycleRegistry(MainActivity.this);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (lockBackKey) {
+            Toast.makeText(this, "Back key was locked", Toast.LENGTH_SHORT).show();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    class JsInterface {
+        private Context mContext;
+        JsInterface(Context context) {
+            mContext = context;
+        }
+
+        @JavascriptInterface
+        public void showToast(String msg) {
+            Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void showToolbar(final boolean showIt) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mToolbar.setVisibility(showIt ? View.VISIBLE : View.GONE);
+                    Toast.makeText(mContext, "Toolbar="+showIt, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void openAndroidDialog(String title, String msg) {
+/*            try {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage(msg)
+                        .setTitle(title);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } catch (Exception e) {
+                Log.e("openAndroidDialog", "Exception: " + e.getMessage());
+                Toast.makeText(mContext, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+ */
+            //必須在主線程中調用，因為它會更新 UI,使用runOnUiThread()方法將handleLifecycleEvent()  方法的調用放到主線程中
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    builder.setMessage(msg)
+                            .setTitle(title);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    } catch (Exception e) {
+                        Log.e("openAndroidDialog", "Exception: " + e.getMessage());
+                        Toast.makeText(mContext, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void lockBackKey(boolean lockIt) {
+            lockBackKey = lockIt;
+            Toast.makeText(mContext, "lockBackKey="+lockIt, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
